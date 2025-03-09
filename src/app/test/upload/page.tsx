4ca69f19a97927ec5f69b01bb1453d27f807d467
Xin Li <bryt.xin.li@gmail.com>
2025-03-09 10:17:22 +0100

    update

--- src/app/test/upload/page.tsx
+++ src/app/test/upload/page.tsx
@@ -5,8 +5,6 @@ import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
 import UploadForm from "@/components/upload_form";
 
-import styles from "./upload.css";
-
 export default async function Home() {
     const command = new PutObjectCommand({
         Key: crypto.randomUUID(),
@@ -15,8 +13,8 @@ export default async function Home() {
     const url = await getSignedUrl(new S3Client({}), command);
 
     return (
-        <div className={styles.page}>
-            <main className={styles.main}>
+        <div>
+            <main>
                 <UploadForm url={url} />
             </main>
         </div>
