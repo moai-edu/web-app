7419729336f809ac571976f38c0be78e1378bdf5
Xin Li <bryt.xin.li@gmail.com>
2025-03-17 13:38:54 +0100

    update

--- src/auth.ts
+++ src/auth.ts
@@ -6,25 +6,16 @@ import { DynamoDB, DynamoDBClientConfig } from "@aws-sdk/client-dynamodb";
 import { DynamoDBDocument } from "@aws-sdk/lib-dynamodb";
 import { DynamoDBAdapter } from "@auth/dynamodb-adapter";
 
-let conf: DynamoDBClientConfig = {};
+const conf: DynamoDBClientConfig = {
+    credentials: {
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
+    },
+    region: process.env.AWS_REGION,
+};
 
 if (process.env.NODE_ENV === "development") {
-    conf = {
-        endpoint: process.env.AWS_ENDPOINT_URL, // LocalStack 的地址
-        region: process.env.AWS_REGION, // 区域
-        credentials: {
-            accessKeyId: process.env.AWS_ACCESS_KEY_ID!, // 访问密钥
-            secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!, // 秘密密钥
-        },
-    };
-} else {
-    conf = {
-        // credentials: {
-        //     accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
-        //     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
-        // },
-        // region: process.env.AWS_REGION,
-    };
+    conf["endpoint"] = process.env.AWS_ENDPOINT_URL;
 }
 
 const client = DynamoDBDocument.from(new DynamoDB(conf), {
--- src/utils/s3_data_client.ts
+++ src/utils/s3_data_client.ts
@@ -1,4 +1,4 @@
-import { S3Client } from "@aws-sdk/client-s3";
+import { S3Client, S3ClientConfig } from "@aws-sdk/client-s3";
 import {
     GetObjectCommand,
     PutObjectCommand,
@@ -23,21 +23,17 @@ export class S3DataClient {
     readonly DATA_BUCKET_NAME = process.env.DATA_BUCKET_NAME;
 
     constructor() {
-        let conf = null;
+        const conf: S3ClientConfig = {
+            region: process.env.AWS_REGION, // 区域
+            credentials: {
+                accessKeyId: process.env.AWS_ACCESS_KEY_ID!, // 访问密钥
+                secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!, // 秘密密钥
+            },
+            forcePathStyle: true, // 使用路径样式访问
+        };
 
         if (process.env.NODE_ENV === "development") {
-            conf = {
-                endpoint: process.env.AWS_ENDPOINT_URL, // LocalStack 的地址
-                region: process.env.AWS_REGION, // 区域
-                credentials: {
-                    accessKeyId: process.env.AWS_ACCESS_KEY_ID!, // 访问密钥
-                    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!, // 秘密密钥
-                },
-                forcePathStyle: true, // 使用路径样式访问
-            };
-        } else {
-            //ci环境下使用环境变量
-            conf = {};
+            conf["endpoint"] = process.env.AWS_ENDPOINT_URL; // LocalStack 的地址
         }
 
         this.s3Client = new S3Client(conf);
