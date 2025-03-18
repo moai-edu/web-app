b94c5eb061105de1a580e1f2a41c0e66b801eef5
Xin Li <bryt.xin.li@gmail.com>
2025-03-18 05:59:37 +0100

    update

--- src/auth.ts
+++ src/auth.ts
@@ -1,33 +1,22 @@
 import NextAuth from "next-auth";
 import Cognito from "next-auth/providers/cognito";
-// import { Resource } from "sst";
+import { Resource } from "sst";
 
 import { DynamoDB, DynamoDBClientConfig } from "@aws-sdk/client-dynamodb";
 import { DynamoDBDocument } from "@aws-sdk/lib-dynamodb";
 import { DynamoDBAdapter } from "@auth/dynamodb-adapter";
 
-let conf: DynamoDBClientConfig = {};
-
+const authDbConf: DynamoDBClientConfig = {
+    credentials: {
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
+    },
+    region: process.env.AWS_REGION!,
+};
 if (process.env.NODE_ENV === "development") {
-    conf = {
-        endpoint: process.env.AWS_ENDPOINT_URL!, // LocalStack 的地址
-        region: process.env.AWS_REGION!, // 区域
-        credentials: {
-            accessKeyId: process.env.AWS_ACCESS_KEY_ID!, // 访问密钥
-            secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!, // 秘密密钥
-        },
-    };
-} else {
-    conf = {
-        credentials: {
-            accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
-            secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
-        },
-        region: process.env.AWS_REGION!,
-    };
+    authDbConf["endpoint"] = process.env.AWS_ENDPOINT_URL!; // LocalStack 的地址
 }
-
-const client = DynamoDBDocument.from(new DynamoDB(conf), {
+const authDbClient = DynamoDBDocument.from(new DynamoDB(authDbConf), {
     marshallOptions: {
         convertEmptyValues: true,
         removeUndefinedValues: true,
@@ -35,23 +24,37 @@ const client = DynamoDBDocument.from(new DynamoDB(conf), {
     },
 });
 
+const cognitoOptions =
+    process.env.NODE_ENV === "development"
+        ? {
+              clientId: process.env.COGNITO_WEB_CLIENT_ID,
+              clientSecret: process.env.COGNITO_WEB_CLIENT_SECRET,
+              issuer:
+                  "https://cognito-idp." +
+                  process.env.NEXT_PUBLIC_REGION +
+                  ".amazonaws.com/" +
+                  process.env.COGNITO_USER_POOL_ID,
+          }
+        : {
+              clientId: Resource.WebClient.id,
+              clientSecret: Resource.WebClient.secret,
+              issuer:
+                  "https://cognito-idp." +
+                  process.env.NEXT_PUBLIC_REGION +
+                  ".amazonaws.com/" +
+                  Resource.UserPool.id,
+          };
+
+const authDbTableName =
+    process.env.NODE_ENV === "development"
+        ? process.env.AUTH_TABLE_NAME
+        : Resource.NextAuthTable.name;
+
 export const { handlers, signIn, signOut, auth } = NextAuth({
     // read here https://authjs.dev/getting-started/deployment#auth_trust_host
     trustHost: true,
     theme: { logo: "https://next-auth.js.org/img/logo/logo-sm.png" },
-    providers: [
-        Cognito({
-            // clientId: Resource.WebClient.id,
-            // clientSecret: Resource.WebClient.secret,
-            clientId: process.env.COGNITO_WEB_CLIENT_ID,
-            clientSecret: process.env.COGNITO_WEB_CLIENT_SECRET,
-            issuer:
-                "https://cognito-idp." +
-                process.env.NEXT_PUBLIC_REGION +
-                ".amazonaws.com/" +
-                process.env.COGNITO_USER_POOL_ID, // Resource.UserPool.id,
-        }),
-    ],
+    providers: [Cognito(cognitoOptions)],
     callbacks: {
         authorized({ request, auth }) {
             try {
@@ -68,7 +71,7 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
             return token;
         },
     },
-    adapter: DynamoDBAdapter(client, {
-        tableName: process.env.AUTH_TABLE_NAME,
+    adapter: DynamoDBAdapter(authDbClient, {
+        tableName: authDbTableName,
     }),
 });
