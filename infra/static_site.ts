f0868dfa860c847710f5e1310f9f3a2f80b2fe5c
Xin Li <bryt.xin.li@gmail.com>
2025-06-01 16:29:34 +0800

    SETUP: 部署static site；修改域名；

--- infra/static_site.ts
+++ infra/static_site.ts
@@ -3,7 +3,11 @@
 // not yet used
 const stage_domain =
     $app.stage === 'prod'
-        ? `${process.env.STATIC_SUBDOMAIN}.${process.env.DOMAIN}`
+        ? {
+              //在prod环境下，创建apex domain，并将静态网站的subdomain重定向到apex domain
+              name: process.env.DOMAIN,
+              redirects: [`${process.env.STATIC_SUBDOMAIN}.${process.env.DOMAIN}`]
+          }
         : `${$app.stage}-${process.env.STATIC_SUBDOMAIN}.${process.env.DOMAIN}`
 
 const static_site_config = {
