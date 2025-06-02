451025089f9bf0b25e90a47f5d11fda55d64f23d
Xin Li <bryt.xin.li@gmail.com>
2025-06-02 16:17:15 +0800

    SETUP: 更新workflow

--- sst.config.ts
+++ sst.config.ts
@@ -17,8 +17,8 @@ export default $config({
             cognitoUserPoolClientId: infra.webClient.id,
             authUrl: infra.authUrl,
             staticSiteUrl: infra.static_site.url,
-            staticSiteS3BucketName: infra.static_site.nodes.assets?.name,
-            staticSiteCloudfrontDistributionId: infra.static_site.nodes.cdn?.nodes.distribution.id
+            staticSiteS3BucketName: infra.static_site.nodes.assets!.name,
+            staticSiteCloudfrontDistributionId: infra.static_site.nodes.cdn!.nodes.distribution.id
         }
     }
 })
