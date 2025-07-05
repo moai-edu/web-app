0a5dccd3ef725cfb5ac795d88ed80c468f4ef908
Xin Li <bryt.xin.li@gmail.com>
2025-07-05 16:09:22 +0800

    SETUP: Add  routes for static site.

--- infra/static_site.ts
+++ infra/static_site.ts
@@ -6,15 +6,23 @@ const stage_domain =
         ? {
               //在prod环境下，创建apex domain，并将静态网站的subdomain重定向到apex domain
               name: `${process.env.DOMAIN}`,
-              redirects: [`${process.env.STATIC_SUBDOMAIN}.${process.env.DOMAIN}`]
+              redirects: [
+                  `${process.env.STATIC_SUBDOMAIN}.${process.env.DOMAIN}`
+              ]
           }
         : `${$app.stage}-${process.env.STATIC_SUBDOMAIN}.${process.env.DOMAIN}`
 
 const static_site_config = {
     domain: stage_domain,
     errorPage: '404.html',
+    assets: {
+        routes: ['uploads']
+    },
     // This directory will be uploaded to S3. The path is relative to your sst.config.ts.
     path: '_site'
 }
 
-export const static_site = new sst.aws.StaticSite('StaticSite', static_site_config)
+export const static_site = new sst.aws.StaticSite(
+    'StaticSite',
+    static_site_config
+)
