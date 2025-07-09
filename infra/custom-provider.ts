c30499c59bc9174278d74656694063b2fbfd8137
Xin Li <bryt.xin.li@gmail.com>
2025-07-09 22:39:10 +0800

    SETUP: init web-app

--- infra/custom-provider.ts
+++ infra/custom-provider.ts
@@ -1,2 +1,4 @@
 const region: string = process.env.SST_AWS_REGION!
-export const customProvider = new aws.Provider('custom-provider', { region })
+export const customProvider = new aws.Provider('custom-provider', {
+    region: `${region}`
+})
