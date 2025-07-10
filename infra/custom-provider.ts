d59c13e596ac0826c4bffcf723840c5983e8c34b
Xin Li <bryt.xin.li@gmail.com>
2025-07-10 11:02:46 +0800

    SETUP: init webapp. test custom region variable

--- infra/custom-provider.ts
+++ infra/custom-provider.ts
@@ -1,5 +1,4 @@
-const region: string = process.env.CUSTOM_PROVIDER_AWS_REGION!
-console.log(`Custom Provider uses region: ${region}`)
+console.log(`Custom Provider uses region: ap-east-1`)
 export const provider = new aws.Provider('CustomProvider', {
-    region: $util.Input.value(region)
+    region: 'ap-east-1'
 })
