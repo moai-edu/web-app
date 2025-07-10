3f607a4a5f6476da9a4cb3e891d0ad176d59f943
Xin Li <bryt.xin.li@gmail.com>
2025-07-10 10:19:16 +0800

    SETUP: init webapp. test custom region variable

--- infra/custom-provider.ts
+++ infra/custom-provider.ts
@@ -1,3 +1,5 @@
+import { Input } from '@pulumi/pulumi'
+
 const region: string = process.env.CUSTOM_PROVIDER_AWS_REGION!
 console.log(`Custom Provider uses region: ${region}`)
 export const provider = new aws.Provider('CustomProvider', {
